5648ab240a89a3c5a985fe1c7ea9d736410316c6
Nicolas Ribeiro <nicolas.ribeiro.s@outlook.com>
2022-07-31 01:45:29 -0300

    feat: add nav bar

--- next.config.js
+++ next.config.js
@@ -1,7 +1,35 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  eslint: {
+    ignoreDuringBuilds: true
+  },
+  cleanDistDir: true,
+  experimental: {
+    images: {
+      allowFutureImage: true
+    }
+  },
+  async redirects() {
+    return [
+      {
+        source: '/add',
+        destination: 'https://discord.com/oauth2/authorize?client_id=704517722100465746&scope=bot+applications.commands&permissions=1346759886',
+        permanent: false
+      },
+      {
+        source: '/support',
+        destination: 'https://discord.gg/bVWdscg',
+        permanent: false
+      },
+      {
+        source: '/vote',
+        destination: 'https://top.gg/bot/704517722100465746/vote',
+        permanent: false
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,30 @@
+import Image from 'next/future/image';
+import Link from 'next/link';
+import logo from '../assets/denky_logo_566x566.png';
+
 export default function IndexRoute() {
   return (
-    <main className="w-full h-screen">
-      <div className="h-screen grid place-content-center">
-        <h1 className="">Coming soon...</h1>
+    <main className="w-full max-w-full max-h-full h-full flex-col">
+      <div className="flex top-0 border-b-4 border-purple-600 justify-center px-5">
+        <div className="flex flex-row items-center w-full space-x-6">
+          <div className="rounded-lg overflow-hidden">
+            <Image className="h-[50px] w-[50px]" src={logo} />
+          </div>
+          <div className="flex flex-row space-x-5">
+            <Link href="/support" passHref>
+              <a className="text-blurple font-semibold">Support</a>
+            </Link>
+
+            <Link href="/vote" passHref>
+              <a className="text-blurple font-semibold">Vote</a>
+            </Link>
+          </div>
+        </div>
+        <div className="flex flex-row items-end justify-center flex-col">
+          <a href="/add" className="bg-[1D1E28] border-2 border-purple-600 px-6 py-2 rounded-lg font-semibold text-white hover:bg-purple-600 transition-all duration-300">
+            Add
+          </a>
+        </div>
       </div>
     </main>
   );
